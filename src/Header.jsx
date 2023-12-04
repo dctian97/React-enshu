7d11f3bb437b427ea1879308887be08c345816cd
dctian97 <zxcv5827458@gmail.com>
2023-12-05 00:11:18 +0900

    cat

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,8 @@
 export default function Header() {
-    return <header><h1>猫ギャラリー</h1></header>;
+    return <header>
+      <h1>猫ギャラリー</h1>
+      <h3>日本大学文理学部情報科学科 Webプログラミングの演習課題</h3>
+      <p>党チンテン 5420045</p>
+    </header>;
   }
   
